76012b12f09679f86a48f7df9eac5784c30095e4
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-04 16:56:59 +0530

    speech is visible in console

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ recognition.interimResults =true;
 let p = document.createElement("p");
 const words = document.querySelector(".words")
 words.appendChild(p)
-
+// 
 // console.log(recognition);
 
 // console.log(words);
 
 recognition.addEventListener("result",e => {
-    console.log(e.results);           //it is giving a  transcript where audio subtitle will be present 
-    const transcript = [...e.results] // converting this to an array as we need to use map to go through each 
-    console.log(transcript);
+    console.log(e);           //it is giving a  transcript where audio subtitle will be present 
+    const transcript = [...e.results ] // converting this to an array as we need to use map to go through each 
+       .map(result => result[0] )
+       .map(result =>result.transcript )
+
+       console.log(transcript);
 }  )
 
 // recognition.start();
