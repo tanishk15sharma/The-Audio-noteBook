9c3653064d482adb2aa1e189dd274e4f64e23f97
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-04 15:19:49 +0530

    initial script added

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 
+ const wordPara = document.querySelector(".words");
+ 
  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+
+ const recognition = new SpeechRecognition();
+ recognition.interimResults = true ;
+
+ let p = document.createElement("p");
+ wordPara.appendChild(p);
